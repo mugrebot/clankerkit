cf2f57d45ba90d2f55a50a51a500f4f5559c3fd5
Mugrebot <mikeongay@gmail.com>
2024-12-11 22:57:27 -0500

    darkmode only

--- packages/nextjs/components/ThemeProvider.tsx
+++ packages/nextjs/components/ThemeProvider.tsx
@@ -2,8 +2,18 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ReactNode } from "react";
 
-export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-};
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="data-theme"
+      defaultTheme="dark"
+      forcedTheme="dark"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
